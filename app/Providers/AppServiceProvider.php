f07b7726c2377b3e25bd1cbc4fe1028d5d81288f
Mladen <djoml8bre@gmail.com>
2017-08-17 21:41:39 +0200

    Vezba 16 - Prikaz postojecih tagova u sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('layouts.master', function ($view){
+
+            $tags = \App\Tag::has('posts')->get();
+
+            $view->with(compact('tags'));
+        });
     }
 
     /**
